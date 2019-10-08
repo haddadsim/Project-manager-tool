372e33640febf51c811e5b1db60d3e623860fe47
Simon <simonenterprise94@gmail.com>
2019-10-08 15:52:14 +0300

    feat: add validation message for project

--- src/main/java/com/projectmanagement/tool/web/ProjectController.java
+++ src/main/java/com/projectmanagement/tool/web/ProjectController.java
@@ -5,22 +5,28 @@ import com.projectmanagement.tool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
 
-    @Autowired
-    private ProjectService projectService;
+  @Autowired private ProjectService projectService;
 
-    @PostMapping("")
-    public ResponseEntity<Project> createNewProject (@RequestBody Project project){
-        // response entity has more control options over json responses
-        projectService.saveOrUpdateProject(project);
-        return new ResponseEntity<>(project, HttpStatus.CREATED);
-    }
+  @PostMapping("")
+  public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+    // response entity has more control options over json responses
+      if(result.hasErrors()){
+          return new ResponseEntity<String>("Invalid Project", HttpStatus.BAD_REQUEST);
+
+      }
+    projectService.saveOrUpdateProject(project);
+    return new ResponseEntity<>(project, HttpStatus.CREATED);
+  }
 }
